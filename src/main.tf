405859a24f1e062523f20b0152bdeda79cfa4c2b
SharanyaGaneshPrasad <sharanya.ganeshprasad@digitalcareerinstitute.org>
2024-11-21 01:38:31 +0100

    Initial commit

--- src/main.tf
+++ src/main.tf
@@ -3,6 +3,15 @@ provider "aws"{
 }
 
 
+terraform {
+  backend "s3" {
+    bucket = "sharanya-terraform-githubactions"
+    key = "terraform.tfstate" #path inside the bucket
+    region = "eu-central-1" #bucket region
+    
+  }
+}
+
 resource "aws_instance" "public_instance" {
  ami           = "ami-0084a47cc718c111a"
  instance_type = "t2.micro"
